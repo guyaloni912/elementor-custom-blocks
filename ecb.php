269f46618e6c233472ebf76fdf6ba69844a8eb54
Guy Aloni <guyaloni912@gmail.com>
2021-11-01 04:54:44 -0700

    changes

--- ecb.php
+++ ecb.php
@@ -12,7 +12,7 @@
 add_filter('pre_set_site_transient_update_plugins', function ($transient) {
 	$base_url = 'https://bitbucket.org/guyaloni912/elementor-custom-blocks/';
 	@$current_version = get_file_data(__FILE__, ['version'])[0];
-	@$new_version = get_file_data('raw/release/ecb.php', ['version'])[0];
+	@$new_version = get_file_data($base_url . 'raw/release/ecb.php', ['version'])[0];
 	if (version_compare($current_version, $new_version) < 0) {
 		$obj = new stdClass();
 		$obj->slug = __FILE__;
