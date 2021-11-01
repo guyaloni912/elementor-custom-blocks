2981d40f95ec77afd48bc59cb81965c37eb0baac
Guy Aloni <guyaloni912@gmail.com>
2021-11-01 05:12:12 -0700

    changes

--- ecb.php
+++ ecb.php
@@ -3,23 +3,25 @@
 /*
   Plugin Name: AloniDev - Elementor Custom Blocks
   Plugin URI: http://www.alonidev.com
-  Version: 1.3.8
+  Version: 1.3.9
   Author: Guy Aloni
   Text Domain: ecb
  */
 
 
 add_filter('pre_set_site_transient_update_plugins', function ($transient) {
+	$basefilename = basename(__FILE__);
+	$basedirname = basename(__DIR__);
 	$base_url = 'https://bitbucket.org/guyaloni912/elementor-custom-blocks/';
-	@$current_version = get_file_data(__FILE__, ['version'])[0];
-	@$new_version = get_file_data($base_url . 'raw/release/ecb.php', ['version'])[0];
+	$current_version = get_file_data(__FILE__, ['version'])[0];
+	$new_version = get_file_data($base_url . 'raw/release/ecb.php', ['version'])[0];
 	if (version_compare($current_version, $new_version) < 0) {
 		$obj = new stdClass();
-		$obj->slug = __FILE__;
+		$obj->slug = $basefilename;
 		$obj->new_version = $new_version;
 		$obj->url = 'http://alonidev.com/';
 		$obj->package = $base_url . 'get/release.zip';
-		$transient->response[__DIR__ . '/' . __FILE__] = $obj;
+		$transient->response[$basedirname . '/' . $basefilename] = $obj;
 	}
 	return $transient;
 });
