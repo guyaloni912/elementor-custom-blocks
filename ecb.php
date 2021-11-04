93e7fe419cec264636ad7ad1f3f7058a501d95ef
Guy Aloni <guyaloni912@gmail.com>
2021-11-04 14:55:22 -0700

    Version 1.0.6

--- ecb.php
+++ ecb.php
@@ -6,7 +6,7 @@ use ElementorCustomBlocks\Services\WidgetService;
 /*
   Plugin Name: AloniDev - Elementor Custom Blocks
   Plugin URI: http://www.alonidev.com
-  Version: 1.0.5
+  Version: 1.0.6
   Author: Guy Aloni
   Text Domain: ecb
  */
